0d4f05eb718caf7e59c8f07c2252e77a12f9b8bf
Dhana Sekhar <k.e.dhanasekhar@gmail.com>
2022-06-29 10:51:34 +0530

    storages

---
+++ storage/index.js
@@ -0,0 +1,51 @@
+//user list
+var usersData = [
+    {
+      name: "brucelee",
+      password: "nevergivesup"
+    },
+    {
+      name: "charliechaplin",
+      password: "alwayshappy"
+    },
+    {
+      name: "wonderwoman",
+      password: "inspirational"
+    },
+    {
+      name: "thanos",
+      password: "willtorule"
+    }
+  ]
+
+
+function  addUser(){
+    
+    var uName = document.querySelector(".name").value
+    var uPass = document.querySelector(".password").value
+
+    for(var i=0;i< usersData.length;i++){
+        if(uName == usersData[i].name && uPass == usersData[i].password){
+          
+           var time=new Date()
+           var name=uName
+
+           var data={
+               Name:name,
+               Time:time
+           }
+           sessionStorage.setItem("loggedName",name)
+           sessionStorage.setItem("loggedTime",time)
+            
+           localStorage.setItem("AlllogedInuser",data)
+
+           window.location.href = "dash.html";
+           
+            
+        }
+
+       
+    }
+
+    
+}
