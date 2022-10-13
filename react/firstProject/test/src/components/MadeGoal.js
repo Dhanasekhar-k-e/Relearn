6189aeb3f5df1aad8b2b2bb7bf064cfbb6095c6b
Dhana Sekhar <k.e.dhanasekhar@gmail.com>
2022-10-13 21:08:18 +0530

    conditionals react

---
+++ react/firstProject/test/src/components/MadeGoal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MadeGoal extends React.Component{
+ 
+    render(){
+        return(
+            <h1>GOAL!</h1>
+        )
+    }
+}
+export default MadeGoal
---
+++ react/firstProject/test/src/components/MissedGoal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MissedGoal extends React.Component{
+ 
+    render(){
+        return(
+            <h1>MISSED</h1>
+        )
+    }
+}
+export default MissedGoal
